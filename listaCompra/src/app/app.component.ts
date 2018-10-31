ca84c984cc70e2b7f94bd334c16f8df885b74467
osmak <osmak@protonmail.com>
2018-10-31 18:13:42 +0100

    Lista de la compra creada en Angular2

---
+++ listaCompra/src/app/app.component.ts
@@ -0,0 +1,44 @@
+import { Component } from '@angular/core';
+
+interface producto {
+	description:string;
+	checked:boolean;
+}
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  nombre;
+  title = 'Lista de la compra';
+  iconoBorrar = 'assets/trash.png'
+  quedan;
+
+  setName(nombre:string){
+    this.nombre=nombre;
+  }
+
+  private productos: producto[] = [];
+  private numItemsMarked: number = 0;
+  
+	addProducto(description: string){
+		this.productos.push({description, checked: false});
+		this.numItemsMarked += 1;
+	}
+
+	marcar(value: number){
+    this.numItemsMarked += value;
+	}
+
+	eliminar(index:number){
+		if(!this.productos[index].checked) this.numItemsMarked -= 1;
+		this.productos.splice(index,1);
+	}
+
+  quedanProductos(){
+    return this.productos.length - (this.productos.length - this.numItemsMarked);
+    
+  }
+}
